6f655215286c837e426c06778dcb9fb102f8b090
ByMyTroth <chooker8454@gmail.com>
2014-09-29 10:09:22 -0500

    copypaste Bstop code

---
+++ js/buspull.js
@@ -0,0 +1,34 @@
+//file connecting the Data Portal API //$ Call for JQuery
+$.getJSON('http://data.cityofchicago.org/resource/5cq6-qygt.json', function(Bstops) {
+    
+
+    $.each(Bstops, function(i, Bstop){
+        $('#Bstoplist').append(generateLStopLink(Bstop));
+    
+});
+    
+    $('#Bstoplist').listview('refresh');
+    
+});
+
+function generateLStopLink(Bstop){
+    
+    return'<li><a href="javascript:void(0)'
+            + '"onclick="goToStopDetailPage(\''
+            + Bstop.stop_name
+            + '\', \''
+            + Bstop.station_descriptive_name + '\")">'
+            + Bstop.stop_name
+            + '</a></li>';
+}
+
+
+function goToStopDetailPage(stopName){
+    var stopPage = $("<div data-role='page' data-url=station><div data-role='header'><h1>"
+                     + stopName + "</h1></div><div data-role='content'><h1>"
+                     + stopName + "</h1></div><data-role='footer'><h4>"
+                     + stopName + "</h4></div></div>");
+    stopPage.appendTo($.mobile.pageContainer );
+    
+    $.mobile.changePage( stopPage );
+}
